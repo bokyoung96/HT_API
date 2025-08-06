3d7b2bebe15b2f67bffc3aa4f38594051aff3799
bkchoi <bokyoung9693@gmail.com>
2025-08-07 08:20:20 +0900

    FEAT: Update readme.md

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ async def main():
             DataFeedBuilder(config, auth, client)
             # .add_stock("005930", timeframe=1, name="Samsung Electronics")
             # .add_deriv("101W09", timeframe=1, name="KOSPI200 Futures")
-            # .add_deriv("106W09", timeframe=1, name="KOSDAQ150 Futures")
+            .add_deriv("106W09", timeframe=1, name="KOSDAQ150 Futures")
             .add_option_chain(
                 maturity="202509",
                 underlying_asset_type="",
